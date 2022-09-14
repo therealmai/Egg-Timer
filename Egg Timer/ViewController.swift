de7bae78d60582f387f14dcfd614982b5b69aaf5
Jomar Leano <jomar35leano@gmail.com>
2022-09-14 18:44:29 +0800

    Switch statements

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let softEgg = 5;
+    let mediumEgg = 7;
+    let hardEgg = 12;
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-   
+    @IBAction func eggButton(_ sender: UIButton) {
+        switch(sender.currentTitle){
+        case "Soft" : print(softEgg)
+        case "Medium" : print(mediumEgg)
+        case "Hard" : print(hardEgg)
+        default : print("null")
+        }
+    }
+    
 }
 
